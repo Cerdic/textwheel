21594c9f58d847c3a8450ccf48ec5d399d9baad1
Cerdic <cedric@yterium.com>
2010-07-25 18:45:07 +0200

    deux fonctions propre et traiter_raccourcis au plus pres de l'existant (en integrant les notes et les tableaux a l'ancienne pour le moment)

--- tw_texte.php
+++ tw_texte.php
@@ -3,26 +3,60 @@
 # usage: php wheels/spip.php
 require_once _DIR_PLUGIN_TW.'engine/textwheel.php';
 
-function tw_raccourcis($t) {
-	static $wheel;
-	if (!isset($wheel))
-		$wheel = new TextWheel(new TextWheelRuleSet('spip/spip.yaml'));
-	return $wheel->text($t);
-}
-function tw_paragrapher($t) {
-	static $wheel;
-	if (!isset($wheel))
-		$wheel = new TextWheel(new TextWheelRuleSet('spip/spip-paragrapher.yaml'));
-	return $wheel->text($t);
-}
-
-function tw_propre($t) {
+function tw_traiter_raccourcis($letexte) {
 	static $wheel;
 	if (!isset($wheel)){
 		$ruleset = new TextWheelRuleSet('spip/spip.yaml');
 		$ruleset->addRules('spip/spip-paragrapher.yaml');
 		$wheel = new TextWheel($ruleset);
 	}
-	return $wheel->text($t);
+	// Appeler les fonctions de pre_traitement
+	$letexte = pipeline('pre_propre', $letexte);
+
+	// Gerer les notes (ne passe pas dans le pipeline)
+	$notes = charger_fonction('notes', 'inc');
+	list($letexte, $mes_notes) = $notes($letexte);
+
+	//
+	// Tableaux
+	//
+
+	// ne pas oublier les tableaux au debut ou a la fin du texte
+	$letexte = preg_replace(",^\n?[|],S", "\n\n|", $letexte);
+	$letexte = preg_replace(",\n\n+[|],S", "\n\n\n\n|", $letexte);
+	$letexte = preg_replace(",[|](\n\n+|\n?$),S", "|\n\n\n\n", $letexte);
+
+	if (preg_match_all(',[^|](\n[|].*[|]\n)[^|],UmsS', $letexte,
+	$regs, PREG_SET_ORDER))
+	foreach ($regs as $t) {
+		$letexte = str_replace($t[1], traiter_tableau($t[1]), $letexte);
+	}
+
+	$letexte = $wheel->text($letexte);
+
+	// Appeler les fonctions de post-traitement
+	$letexte = pipeline('post_propre', $letexte);
+
+	if ($mes_notes) $notes($mes_notes);
+
+	return $letexte;
 }
 
+function tw_propre($t, $connect=null) {
+	// les appels directs a cette fonction depuis le php de l'espace
+	// prive etant historiquement ecrits sans argment $connect
+	// on utilise la presence de celui-ci pour distinguer les cas
+	// ou il faut passer interdire_script explicitement
+	// les appels dans les squelettes (de l'espace prive) fournissant un $connect
+	// ne seront pas perturbes
+	$interdire_script = false;
+	if (is_null($connect)){
+		$connect = '';
+		$interdire_script = true;
+	}
+
+	return !$t ? strval($t) :
+		echappe_retour_modeles(
+			tw_traiter_raccourcis(
+				expanser_liens(echappe_html($t),$connect)),$interdire_script);
+}
