e47faca398d976858e1d31204993ffc818cb7c7a
Cerdic <cedric@yterium.com>
2010-07-25 12:31:28 +0200

    oups, forgotten file

--- tw_texte.php
+++ tw_texte.php
@@ -1,83 +1,28 @@
 <?php
 
 # usage: php wheels/spip.php
-
 require_once 'engine/textwheel.php';
 
-function ruleset($file) {
-	require_once 'lib/yaml/sfYaml.php';
-	return sfYaml::load('wheels/spip/'.$file);
-}
-
-function raccourcis($t) {
+function tw_raccourcis($t) {
 	static $wheel;
 	if (!isset($wheel))
-		$wheel = new TextWheel(ruleset('spip.yaml'));
+		$wheel = new TextWheel(new TextWheelRuleSet('spip.yaml'));
 	return $wheel->text($t);
 }
-function paragrapher($t) {
+function tw_paragrapher($t) {
 	static $wheel;
 	if (!isset($wheel))
-		$wheel = new TextWheel(ruleset('spip-paragrapher.yaml'));
+		$wheel = new TextWheel(new TextWheelRuleSet('spip-paragrapher.yaml'));
 	return $wheel->text($t);
 }
 
-// callback pour poesie
-// (a remplacer par une sous-liste = indentation de yaml ?)
-function replace_poesie($m) {
+function tw_propre($t) {
 	static $wheel;
-	if (!isset($wheel))
-		$wheel = new TextWheel(ruleset('spip-poesie.yaml'));
-	return $wheel->text($m[2]);
-}
-
-/*
-function propre($t) {
-	static $wheel;
-	if (!isset($wheel)) {
-		$wheel = new TextWheel(ruleset('spip.yaml'));
-		$wheel->addRules(ruleset('spip-paragrapher.yaml'));
+	if (!isset($wheel)){
+		$ruleset = new TextWheelRuleSet('spip.yaml');
+		$ruleset->addRules('spip-paragrapher.yaml');
+		$wheel = new TextWheel($ruleset);
 	}
-	return $wheel->text($t);
-}
-*/
-
-function propre($t) {
-	return yaml_wheel($t, array('spip.yaml', 'spip-paragrapher.yaml'));
+	return $wheel->test($t);
 }
 
-function yaml_wheel($t, $yaml=array()) {
-	static $wheels;
-	$co = crc32(serialize($yaml));
-	if (!isset($wheels[$co])) {
-		$wheels[$co] = new TextWheel();
-		foreach ($yaml as $f)
-			$wheels[$co]->addRules(ruleset($f));
-	}
-	return $wheels[$co]->text($t);
-}
-
-
-$texte = 	'
-
-
-joli :{{{o}}}
-	<poetry>
-	I love
-	you
-	my love
-	</poetry>
-
-----
-
-- puce
-_ br
-	'
-;
-
-echo propre($texte);
-
-#echo paragrapher(raccourcis($texte));
-
-
-#var_dump(ruleset('spip-paragrapher.yaml'));
