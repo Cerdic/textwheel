a2c55bcb469bfb2ed79e77febc623ca769d30d2e
Fil <fil@rezo.net>
2010-08-06 09:43:19 +0200

    paragrapher a la main, ca va deux fois plus vite

--- wheels/spip/spip.php
+++ wheels/spip/spip.php
@@ -13,3 +13,30 @@ function replace_puce(){
 		$puce = "\n<br />".definir_puce()."&nbsp;";
 	return $puce;
 }
+
+/**
+ * callback fermer-para-mano
+ * on refait le preg, a la main
+ */
+function fermer_para_mano(&$t) {
+	# match: ",<p (.*)<(/?)(STOP P|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|body|foot|extarea)|form|object|center|marquee|address|d[ltd]|script|noscript|map|button|fieldset|style)\b,UimsS"
+	# replace: "\n<p "+trim(\1)+"</p>\n<\2\3"
+
+	foreach (explode('<p ', $t) as $c => $p) {
+		if ($c == 0)
+			$t = $p;
+		else {
+			$pi = strtolower($p);
+			if (preg_match(
+			",</?(?:stop p|div|pre|ul|ol|li|blockquote|h[1-6r]|t(able|[rdh]|body|foot|extarea)|form|object|center|marquee|address|d[ltd]|script|noscript|map|button|fieldset|style)\b,S",
+			$pi, $r)) {
+				$pos = strpos($pi, $r[0]);
+				$t .= "\n<p ".rtrim(substr($p,0,$pos))."</p>\n".substr($p,$pos);
+			} else {
+				$t .= '<p '.$p;
+			}
+		}
+	}
+
+	return $t;
+}
