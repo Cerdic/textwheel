b9a905e9e08cc6b1780e403df70e6cb0cb9f2e68
Fil <fil@rezo.net>
2010-08-12 17:32:31 +0200

    compatibilite iso-8859-1

--- typographie/fr.php
+++ typographie/fr.php
@@ -23,20 +23,20 @@ function typographie_fr($letexte) {
 		return typographie_fr_dist($letexte);
 	}
 
-	#$debug = _request('var_debug_wheel');
+	$debug = _request('var_debug_wheel');
 
 	static $trans;
 
 	if (!isset($trans)) {
-		switch ($GLOBALS['meta']['charset'] == 'utf-8') {
+		switch ($GLOBALS['meta']['charset']) {
 			case 'utf-8':
 				$trans = array(
-				"\x26\x6e\x62\x73\x70\x3b" => '~',
-				"\x26\x72\x61\x71\x75\x6f\x3b" => '&#187;',
-				"\x26\x6c\x61\x71\x75\x6f\x3b" => '&#171;',
-				"\x26\x72\x64\x71\x75\x6f\x3b" => '&#8221;',
-				"\x26\x6c\x64\x71\x75\x6f\x3b" => '&#8220;',
-				"\x26\x64\x65\x67\x3b" => '&#176;',
+				"&nbsp;" => '~',
+				"&raquo;" => '&#187;',
+				"&laquo;" => '&#171;',
+				"&rdquo;" => '&#8221;',
+				"&ldquo;" => '&#8220;',
+				"&deg" => '&#176;',
 				"\xc2\xa0" => '~',
 				"\xc2\xbb" => '&#187;',
 				"\xc2\xab" => '&#171;',
@@ -47,16 +47,16 @@ function typographie_fr($letexte) {
 				break;
 			case 'iso-8859-1':
 				$trans = array(
-				"\x26\x6e\x62\x73\x70\x3b" => '~',
-				"\x26\x72\x61\x71\x75\x6f\x3b" => '&#187;',
-				"\x26\x6c\x61\x71\x75\x6f\x3b" => '&#171;',
-				"\x26\x72\x64\x71\x75\x6f\x3b" => '&#8221;',
-				"\x26\x6c\x64\x71\x75\x6f\x3b" => '&#8220;',
-				"\x26\x64\x65\x67\x3b" => '&#176;',
-				"\x20" => '~',
-				"\xbb" => '&#187;',
-				"\xab" => '&#171;',
-				"\xb0" => '&#176;'
+				"&nbsp;" => '~',
+				"&raquo;" => '&#187;',
+				"&laquo;" => '&#171;',
+				"&rdquo;" => '&#8221;',
+				"&ldquo;" => '&#8220;',
+				"&deg" => '&#176;',
+				"\xa0" => '~', # insecable
+				"\xab" => '&#171;', # guill fr ouvrant
+				"\xbb" => '&#187;', # guill fr fermant
+				"\xb0" => '&#176;' # degre
 				);
 				break;
 			default:
